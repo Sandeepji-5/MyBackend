1122c00d6cce7b68dc1f764d69c59a78d52fa563
Sandeepji-5 <sandeepvishwakarmagkp5@gmail.com>
2025-04-01 19:20:26 +0530

    add new user routes for password change, account updates, avatar and cover image uploads, and retrieving channel profile and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,10 +4,17 @@ import {
         logoutUser,
         registerUser,
         refreshAccessToken,
+        changedCurrentPassword,
+        updateAccountDetails,
+        updateUserAvatar,
+        updateUserCoverImage,
+        getUserChannelProfile,
+        getWatchHistory
         
      } from "../controllers/user.controller.js"; // Import the controller function
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
+
 
 const router = Router();
 
@@ -24,10 +31,16 @@ router.route("/register").post(
 registerUser); // Use the imported function
 
 router.route("/login").post(loginUser)
-
-// secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
-
 router.route("/refresh-token").post( refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changedCurrentPassword)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
+
+
 
 export default router; 
+
